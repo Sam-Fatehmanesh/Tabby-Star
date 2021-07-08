e0e111e6b565f443089cf6add40dd9d037106c70
Singularity1001 <galaxi3600@gmail.com>
2021-07-07 19:30:00 -0700

    added boundries

--- main.py
+++ main.py
@@ -4,9 +4,19 @@ from IPython.display import Image
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 
-hdu_list = fits.open('tfn0m410-kb98-20210707-0126-e00.fits.fz')
-#hdu_list = fits.open('tfn0m410-kb98-20210707-0126-e91.fits.fz')
+hdu_list = fits.open('tfn0m410-kb98-20210707-0126-e91.fits.fz')
 im = hdu_list[1].data
+
+cenx = len(im) 
+ceny = len(im[0])
+boundrysize = 100
+Xmax = cenx+boundrysize
+Xmin = cenx-boundrysize
+Ymax = ceny+boundrysize
+Ymin = ceny-boundrysize
+
+im = im[Xmin:Xmax,Ymin:Ymax]
+
 plt.figure()
-plt.imshow(im, norm=LogNorm(), cmap='Greys_r')
+plt.imshow(im, norm=LogNorm(), cmap='Reds')
 plt.show()
